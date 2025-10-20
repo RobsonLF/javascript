1855c557d0e6746c60d45cce552ddef70c8fc018
Robson Ferreira <robsonldfer@gmail.com>
2025-10-20 12:33:49 -0300

    Add

---
+++ aulas/introdução/script.js
@@ -0,0 +1,39 @@
+var a = 2
+var b = 3
+
+if (b%2 == 1) {
+    alert("O número " + b + " é ímpar.");
+}
+else {
+    alert("O número " + b + " é par.");
+}
+
+
+
+
+
+
+
+
+
+
+
+/*var mensagem = "Olá mundo!";
+var a = 2;
+var b = 3;
+var soma = a + b;
+var subtracao = b - a;
+var multiplicacao = a * b;
+var divisao = b / a;
+alert(mensagem);
+alert(soma);
+alert(subtracao);
+alert(multiplicacao);
+alert(divisao);
+
+
+// comentário em JavaScript
+ 
+  comentário em 
+  múltiplas linhas
+*/
